5da38688a75b75a088e0d791770dd1801574e24b
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2024-01-03 21:59:21 -0500

    Finished canvas code

--- canvas.go
+++ canvas.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+const (
+	COLOR_MIN = 0
+	COLOR_MAX = 255
+)
+
 type Canvas struct {
 	Pixels [][]Color
 	Width  int64
@@ -24,4 +35,60 @@ func pixelAt(c Canvas, x int64, y int64) Color {
 
 func writePixel(c Canvas, x int64, y int64, color Color) {
 	c.Pixels[y][x] = color
+}
+
+func scaleColorDimension(f float64) int64 {
+	if f <= 0 {
+		return COLOR_MIN
+	}
+	if f >= 1 {
+		return COLOR_MAX
+	}
+
+	return int64(math.Round((COLOR_MAX - COLOR_MIN) * f))
+}
+
+func canvasToPPM(c Canvas) string {
+	b := strings.Builder{}
+	b.WriteString("P3\n")
+	b.WriteString(fmt.Sprintf("%d %d\n", c.Width, c.Height))
+	b.WriteString(fmt.Sprintf("%d\n", COLOR_MAX))
+
+	for _, v := range c.Pixels {
+		line := ""
+		for _, u := range v {
+			red := fmt.Sprintf("%d ", scaleColorDimension(u.Red))
+			if len(line)+len(red) > 70 {
+				line = strings.TrimRight(line, " ")
+				line += "\n"
+				b.WriteString(line)
+				line = ""
+			}
+			line += red
+
+			green := fmt.Sprintf("%d ", scaleColorDimension(u.Green))
+			if len(line)+len(green) > 70 {
+				line = strings.TrimRight(line, " ")
+				line += "\n"
+				b.WriteString(line)
+				line = ""
+			}
+			line += green
+
+			blue := fmt.Sprintf("%d ", scaleColorDimension(u.Blue))
+			if len(line)+len(blue) > 70 {
+				line = strings.TrimRight(line, " ")
+				line += "\n"
+				b.WriteString(line)
+				line = ""
+			}
+			line += blue
+
+		}
+		b.WriteString(strings.TrimRight(line, " "))
+		b.WriteString("\n")
+	}
+
+	b.WriteString("\n")
+	return b.String()
 }
--- canvas_test.go
+++ canvas_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCreateCanvas(t *testing.T) {
 	c := canvas(10, 20)
@@ -29,5 +32,81 @@ func TestWritePixel(t *testing.T) {
 
 	if !colorEqual(pixelAt(c, 2, 3), red) {
 		t.Errorf("Expected pixel at %d,%d to be %v but got %v", 2, 3, red, c.Pixels[3][2])
+	}
+}
+
+func TestPPMHeader(t *testing.T) {
+	c := canvas(5, 3)
+	p := canvasToPPM(c)
+	lines := strings.Split(p, "\n")
+
+	if lines[0] != "P3" {
+		t.Errorf("Expected line 0 to be %s but got %s", "P3", lines[0])
+	}
+	if lines[1] != "5 3" {
+		t.Errorf("Expected line 1 to be %s but got %s", "5 3", lines[1])
+	}
+	if lines[2] != "255" {
+		t.Errorf("Expected line 2 to be %s but got %s", "255", lines[2])
+	}
+}
+
+func TestPPMData(t *testing.T) {
+	c := canvas(5, 3)
+	c1 := Color{1.5, 0, 0}
+	c2 := Color{0, 0.5, 0}
+	c3 := Color{-0.5, 0, 1}
+
+	writePixel(c, 0, 0, c1)
+	writePixel(c, 2, 1, c2)
+	writePixel(c, 4, 2, c3)
+
+	p := canvasToPPM(c)
+	lines := strings.Split(p, "\n")
+
+	if lines[3] != "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0" {
+		t.Errorf("Expected line 3 to be %s but got %s", "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0", lines[3])
+	}
+	if lines[4] != "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0" {
+		t.Errorf("Expected line 4 to be %s but got %s", "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0", lines[4])
+	}
+	if lines[5] != "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255" {
+		t.Errorf("Expected line 5 to be %s but got %s", "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255", lines[5])
+	}
+}
+
+func Test70MaxLineLength(t *testing.T) {
+	c := canvas(10, 2)
+	color := Color{1, 0.8, 0.6}
+
+	for i := range c.Pixels {
+		for j := range c.Pixels[i] {
+			writePixel(c, int64(j), int64(i), color)
+		}
+	}
+
+	p := canvasToPPM(c)
+
+	lines := strings.Split(p, "\n")
+
+	if lines[3] != "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204" {
+		t.Errorf("Expected line 3 to be %s but got %s", "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204", lines[3])
+	}
+	if lines[4] != "153 255 204 153 255 204 153 255 204 153 255 204 153" {
+		t.Errorf("Expected line 4 to be %s but got %s", "153 255 204 153 255 204 153 255 204 153 255 204 153", lines[4])
+	}
+	if lines[5] != "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204" {
+		t.Errorf("Expected line 5 to be %s but got %s", "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204", lines[5])
+	}
+	if lines[6] != "153 255 204 153 255 204 153 255 204 153 255 204 153" {
+		t.Errorf("Expected line 6 to be %s but got %s", "153 255 204 153 255 204 153 255 204 153 255 204 153", lines[6])
+	}
+}
+
+func TestTerminateWithNewline(t *testing.T) {
+	c := canvas(5, 3)
+	ppm := canvasToPPM(c)
+	if ppm[len(ppm)-1] != '\n' {
+		t.Errorf("Expected ppm to end with \n but got %b", ppm[len(ppm)-1])
 	}
 }
