2c5b8d0a655ada055ce2d297c897cc8caf5ef91e
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2024-01-05 16:21:26 -0500

    Did clock exercise

--- main.go
+++ main.go
@@ -23,22 +23,55 @@ func tick(e Environment, p Projectile) Projectile {
 	return Projectile{pos, vel}
 }
 
+func writePixel3x3(c Canvas, x int64, y int64, col Color) {
+	writePixel(c, x, y, col)
+	writePixel(c, x-1, y, col)
+	writePixel(c, x+1, y, col)
+	writePixel(c, x, y-1, col)
+	writePixel(c, x-1, y-1, col)
+	writePixel(c, x+1, y-1, col)
+	writePixel(c, x, y+1, col)
+	writePixel(c, x-1, y+1, col)
+	writePixel(c, x+1, y+1, col)
+}
+
+func writePixel9x9(c Canvas, x int64, y int64, col Color) {
+	writePixel3x3(c, x, y, col)
+	writePixel3x3(c, x-3, y, col)
+	writePixel3x3(c, x+3, y, col)
+	writePixel3x3(c, x, y-3, col)
+	writePixel3x3(c, x-3, y-3, col)
+	writePixel3x3(c, x+3, y-3, col)
+	writePixel3x3(c, x, y+3, col)
+	writePixel3x3(c, x-3, y+3, col)
+	writePixel3x3(c, x+3, y+3, col)
+}
+
 func main() {
-	start := point(0, 1, 0)
-	vel := tupleScale(vectorNormalize(vector(1, 1.8, 0)), 11.25)
-	p := Projectile{start, vel}
-	e := Environment{vector(0, -0.1, 0), vector(-0.01, 0, 0)}
-	t := 0
-
 	color := Color{1, 0, 0}
-	c := canvas(900, 550)
-	for p.Position.Y > 0.0 {
-		fmt.Printf("tick: %d, pos: %v\n", t, p.Position)
-		p = tick(e, p)
+	c := canvas(1000, 1000)
+	radius := 400.
 
-		writePixel(c, int64(math.Round(p.Position.X)), int64(550-math.Round(p.Position.Y)), color)
+	origin := point(0, 0, 0)
+	moveOriginToTwelve := translation(0, -radius, 0)
+	moveOriginToCenter := translation(499, 499, 0)
+
+	for i := 0; i < 12; i++ {
+		t, err := transformation(
+			moveOriginToTwelve,
+			rotation_z(float64(i)*math.Pi/6.),
+			moveOriginToCenter)
+		if err != nil {
+			os.Exit(1)
+		}
+		p, err := matrix4x4TupleMultiply(t, origin)
+		if err != nil {
+			os.Exit(1)
+		}
+		fmt.Printf("%d: %v\n", i, p)
+		writePixel9x9(c, int64(p.X), int64(p.Y), color)
 	}
 
 	ppm := canvasToPPM(c)
-	os.WriteFile("example.ppm", []byte(ppm), 0644)
+	os.WriteFile("clock.ppm", []byte(ppm), 0644)
 }
